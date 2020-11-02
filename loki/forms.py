9807a09bde2fcbfd4a65373ce0490c7e0d8f5c8b
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-11-02 22:08:33 +0100

    Add account editing functionality.

--- loki/forms.py
+++ loki/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from wtforms.validators import ValidationError
+
+from flask_login import current_user
 
 from loki.models import User
 
@@ -41,3 +45,28 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log in')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                                       Length(min=2, max=15)])
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Email()])
+    image = FileField('Update Profile Picture',
+                      validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Account with email already exists!')
