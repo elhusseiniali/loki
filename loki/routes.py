1b299b58f9d27936d743cf9449b0b63b7faa734d
Michaël Resplandy <mmresplandy@gmail.com>
2020-11-05 10:22:50 +0100

    fixing registration bug

--- loki/routes.py
+++ loki/routes.py
@@ -25,11 +25,9 @@ def register():
 
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)\
-                                .decode('utf-8')
         user = User(username=form.username.data,
                     email=form.email.data,
-                    password=hashed_password)
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
@@ -49,8 +47,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password,
-                                               form.password.data):
+        if user.verify_password(form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page \
