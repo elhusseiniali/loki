8de7c69425f19f2c01b33feea867a1d410d0bd10
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-11-09 19:54:55 +0100

    Add tests to check that some pages are reachable.

--- loki/tests/test_general.py
+++ loki/tests/test_general.py
@@ -13,6 +13,13 @@ class FlaskTestCase(unittest.TestCase):
         login = tester.get('/login', content_type='html/text')
         self.assertEqual(login.status_code, 200)
 
+        register = tester.get('/register', content_type='html/text')
+        self.assertEqual(register.status_code, 200)
+
+        account = tester.get('/account', content_type='html/text')
+        #   Can't access account page without valid login
+        self.assertEqual(account.status_code, 302)
+
 
 if __name__ == "__main__":
     unittest.main()
