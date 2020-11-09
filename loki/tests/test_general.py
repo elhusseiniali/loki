348bb034bec76b56e09c9d2871c148ab25ab3794
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2020-11-09 21:06:22 +0100

    Add tests for login and registration.

--- loki/tests/test_general.py
+++ loki/tests/test_general.py
@@ -3,6 +3,30 @@ import unittest
 from loki import app
 
 
+class Helper():
+    def register(app, username, email, password, confirm_password):
+        return app.post(
+            '/register',
+            data=dict(username=username,
+                      email=email,
+                      password=password, confirm_password=confirm_password),
+            follow_redirects=True
+        )
+
+    def login(app, email, password):
+        return app.post(
+            '/login',
+            data=dict(email=email, password=password),
+            follow_redirects=True
+        )
+
+    def logout(app):
+        return app.get(
+            '/logout',
+            follow_redirects=True
+        )
+
+
 class FlaskTestCase(unittest.TestCase):
     def test_pages_load(self):
         tester = app.test_client(self)
@@ -19,6 +43,23 @@ class FlaskTestCase(unittest.TestCase):
         account = tester.get('/account', content_type='html/text')
         #   Can't access account page without valid login
         self.assertEqual(account.status_code, 302)
+
+    def test_auth(self):
+        tester = app.test_client(self)
+        valid_registration = Helper.register(tester, username="frege",
+                                             email="frege@gmail.com",
+                                             password="whereisrussell",
+                                             confirm_password="whereisrussell")
+        self.assertEqual(valid_registration.status_code, 200)
+
+        valid_login = Helper.login(tester, email="frege@gmail.com",
+                                   password="whereisrussell")
+        self.assertEqual(valid_login.status_code, 200)
+
+        # invalid_login = Helper.login(tester, email="russell@gmail.com",
+        #                             password="fregewasagenius")
+        # self.assertIn(b'Login unsuccessful!.', invalid_login.data)
+        # self.assertEqual(invalid_login.status_code, 200)
 
 
 if __name__ == "__main__":
