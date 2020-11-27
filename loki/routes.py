ca7e3290fc17142b7f44f186bfb6c5bcba2d4315
Michaël Resplandy <mmresplandy@gmail.com>
2020-11-27 08:29:49 +0100

    changing all html files to use that new html template, and additional changes on templates.html

--- loki/routes.py
+++ loki/routes.py
@@ -20,7 +20,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('template.html', title='Home')
 
 
 @app.route("/about")
