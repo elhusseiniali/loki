622283c12ca7fc7ea38785822d03be0b8b7736f9
= <elhusseiniali@icloud.com>
2020-12-26 18:22:53 +0100

    Move model utils to model blueprints.

--- loki/utils.py
+++ loki/utils.py
@@ -5,39 +5,6 @@ from PIL import Image
 import PIL
 
 
-def save_model(form_model):
-    """Save model file under /static/models.
-
-    Parameters
-    ----------
-    form_model : [file]
-
-    Returns
-    -------
-    [str]
-        A random hex is generated as the new file name to prevent
-        any errors that would arise on the filesystem when filename uniqueness
-        is violated.
-    """
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_model.filename)
-    model_fn = random_hex + f_ext
-    model_path = os.path.join(current_app.root_path, 'static/models', model_fn)
-    form_model.save(model_path)
-    return model_path
-
-
-def remove_model(model_path):
-    """Remove file at model_path.
-
-    Parameters
-    ----------
-    model_path : [str]
-    """
-    if os.path.exists(model_path):
-        os.remove(model_path)
-
-
 def save_image(source_image, path, output_size=(125, 125)):
     """Compress and save user-uploaded images to the filesystem.
 
