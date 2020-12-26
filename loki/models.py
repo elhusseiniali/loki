24ffd463236c1dd8824d173f1956d61f978f3fe5
= <elhusseiniali@icloud.com>
2020-12-26 18:20:33 +0100

    Move User tokens to another branch.

--- loki/models.py
+++ loki/models.py
@@ -1,4 +1,4 @@
-from loki import db, login_manager, bcrypt
+from loki import app, db, login_manager, bcrypt
 from flask_login import UserMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from flask_validator import ValidateEmail
@@ -58,13 +58,6 @@ class User(db.Model, UserMixin):
     _password = db.Column(db.String(128), nullable=False)
 
     models = db.relationship("Classifier", back_populates="user")
-
-    def __init__(self,
-                 username, email,
-                 password):
-        self.username = username
-        self.email = email
-        self.password = password
 
     def __repr__(self):
         return (f"User('{self.username}': '{self.email}')")
