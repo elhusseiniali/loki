e4549e11d1b78134e0dd01ffc7cd6a88da52e9e3
Ali El Husseini <elhusseiniali@icloud.com>
2021-01-08 04:10:43 +0100

    Modify the way the app is run to work with the Application Factory.

--- reset.py
+++ reset.py
@@ -1,4 +1,9 @@
-from loki import db
+from loki import create_app, db
 
-db.drop_all()
-db.create_all()
+app = create_app()
+
+db.init_app(app)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
