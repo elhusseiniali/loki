703577558962e2be54aafd47a8bb62ad967ba77c
Michaël Resplandy <mmresplandy@gmail.com>
2021-01-08 14:36:50 +0100

    updating home page

--- loki/api/main/routes.py
+++ loki/api/main/routes.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, render_template
-
-
+from loki.dao.datasets import datasets
+from loki.api.classifiers.models import pretrained_classifiers
+from loki.api.attacks.models import attacks
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home',
+                           datasets=datasets,
+                           classifiers=pretrained_classifiers,
+                           attacks=attacks,
+                           )
 
 
 @main.route("/about")
