61eaf0db254d7f4dcf2e7f79d6fb4dfc122158d5
Ali El Husseini <elhusseiniali@icloud.com>
2021-01-09 05:01:44 +0100

    Add ReportDAO.

---
+++ loki/dao/reports.py
@@ -0,0 +1,34 @@
+from loki.models import Report
+from loki import db
+
+
+class ReportDAO():
+    __instance__ = None
+
+    def __init__(self):
+        if ReportDAO.__instance__ is None:
+            ReportDAO.__instance__ = self
+        else:
+            raise Exception("You cannot create another ReportDAO class")
+
+    @staticmethod
+    def get_instance():
+        if not ReportDAO.__instance__:
+            ReportDAO()
+        return ReportDAO.__instance__
+
+    def add(self, report):
+        db.session.add(report)
+        db.session.commit()
+
+    def get_all(self):
+        return db.session.query(Report).all()
+
+    def get_by_report_id(self, report_id):
+        return db.session.query(Report).get(report_id)
+
+    def get_by_user_id(self, user_id):
+        return db.session.query(Report).filter_by(user_id).first()
+
+
+report_dao = ReportDAO()
