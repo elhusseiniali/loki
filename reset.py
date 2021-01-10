2e3e17d286f5ab4aa0c91f47143b375d6e9de5e8
Ali El Husseini <elhusseiniali@icloud.com>
2021-01-10 03:15:50 +0100

    Add runtime guard to prevent auto execution.

--- reset.py
+++ reset.py
@@ -1,9 +1,10 @@
 from loki import create_app, db
 
-app = create_app()
 
-db.init_app(app)
+if __name__ == '__main__':
+    app = create_app()
+    db.init_app(app)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
